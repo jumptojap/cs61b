c96a34f04fc2b4cec698e1b1eb016bda23ca3743
jumptojap <zhuz3856@gmail.com>
2024-09-18 21:18:41 +0800

    s

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,10 +1,94 @@
 package hw4.puzzle;
 
-public class Board {
+import edu.princeton.cs.algs4.Queue;
+
+public class Board implements WorldState {
+    private int[][] tiles;
+    public Board(int[][] tiles){
+        this.tiles = tiles;
+    }
+    public int tileAt(int i, int j){
+        if(i < 0 || i >= tiles.length || j < 0 || j >= tiles.length){
+            throw new IndexOutOfBoundsException();
+        }
+        return tiles[i][j];
+    }
+    public int size(){
+        return tiles.length;
+    }
+    public Iterable<WorldState> neighbors() {
+        Queue<WorldState> neighbors = new Queue<>();
+        int hug = size();
+        int bug = -1;
+        int zug = -1;
+        for (int rug = 0; rug < hug; rug++) {
+            for (int tug = 0; tug < hug; tug++) {
+                if (tileAt(rug, tug) == 0) {
+                    bug = rug;
+                    zug = tug;
+                }
+            }
+        }
+        int[][] ili1li1 = new int[hug][hug];
+        for (int pug = 0; pug < hug; pug++) {
+            for (int yug = 0; yug < hug; yug++) {
+                ili1li1[pug][yug] = tileAt(pug, yug);
+            }
+        }
+        for (int l11il = 0; l11il < hug; l11il++) {
+            for (int lil1il1 = 0; lil1il1 < hug; lil1il1++) {
+                if (Math.abs(-bug + l11il) + Math.abs(lil1il1 - zug) - 1 == 0) {
+                    ili1li1[bug][zug] = ili1li1[l11il][lil1il1];
+                    ili1li1[l11il][lil1il1] = 0;
+                    Board neighbor = new Board(ili1li1);
+                    neighbors.enqueue(neighbor);
+                    ili1li1[l11il][lil1il1] = ili1li1[bug][zug];
+                    ili1li1[bug][zug] = 0;
+                }
+            }
+        }
+        return neighbors;
+    }
+    public int hamming(){
+        int res = 0;
+        for(int i = 0; i < tiles.length; i++){
+            for(int j = 0; j < tiles.length; j++){
+                if(tiles[i][j] != i * tiles.length + j + 1){
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+    public int manhattan(){
+        int res = 0;
+        for(int i = 0; i < tiles.length; i++){
+            for(int j = 0; j < tiles.length; j++){
+                int val = tileAt(i, j);
+                int row = val / tiles.length;
+                int col = val % tiles.length;
+                res = res + Math.abs(row - i) + Math.abs(col - j);
+            }
+        }
+        return res;
+    }
+    public int estimatedDistanceToGoal(){
+        return manhattan();
+    }
+    public boolean equals(Object y){
+        for(int i = 0; i < tiles.length; i++){
+            for(int j = 0; j < tiles[i].length; j++){
+                if(tiles[i][j] != ((Board)y).tileAt(i, j)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     /** Returns the string representation of the board. 
       * Uncomment this method. */
-    /*public String toString() {
+    public String toString() {
         StringBuilder s = new StringBuilder();
         int N = size();
         s.append(N + "\n");
@@ -16,6 +100,6 @@ public class Board {
         }
         s.append("\n");
         return s.toString();
-    }*/
+    }
 
 }
---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,91 @@
+package hw4.puzzle;
+
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.*;
+
+/**
+ * ClassName: Solver
+ * Package: hw4.puzzle
+ * Description:
+ *
+ * @Author: east_moon
+ * @Create: 2024/9/18 - 18:23
+ * Version: v1.0
+ */
+public class Solver {
+    private WorldState initialState;
+    private Deque<WorldState> results;
+    private int numOfMove;
+    Map<WorldState, Integer> dis;
+    public Solver(WorldState initial){
+        initialState = initial;
+        numOfMove = -1;
+        results = null;
+        dis = new HashMap<>();
+        solution();
+    }
+    public int moves(){
+        return numOfMove;
+    }
+    private class Node implements Comparable<Node>{
+        @Override
+        public int compareTo(Node o) {
+            int temp1, temp2;
+            if(dis.containsKey(this.state)){
+                temp1 = dis.get(this.state);
+            }else{
+                temp1 = this.state.estimatedDistanceToGoal();
+            }
+            if(dis.containsKey(o.state)){
+                temp2 = dis.get(o.state);
+            }else{
+                temp2 = o.state.estimatedDistanceToGoal();
+            }
+            return this.moves + temp1 - temp2 - o.moves;
+        }
+
+        WorldState state;
+        int moves;
+        Node parent;
+
+        public Node(WorldState state, int moves, Node parent) {
+            this.state = state;
+            this.moves = moves;
+            this.parent = parent;
+        }
+    }
+    public Iterable<WorldState> solution(){
+        if(results != null) return results;
+        Node first = new Node(initialState, 0, null);
+        MinPQ<Node> pq = new MinPQ<>();
+        Map<WorldState, Integer> mp = new HashMap<>();
+        Set<WorldState> close = new HashSet<>();
+        pq.insert(first);
+        mp.put(initialState, 0);
+        results = new LinkedList<>();
+        while(!pq.isEmpty()){
+            Node cur = pq.delMin();
+
+            close.add(cur.state);
+            if(cur.state.isGoal()){
+                numOfMove = cur.moves;
+                Node temp = cur;
+                while(!temp.state.equals(initialState)){
+                    results.addFirst(temp.state);
+                    temp = temp.parent;
+                }
+                return results;
+            }
+            for(WorldState next : cur.state.neighbors()){
+                Node temp = new Node(next, cur.moves + 1, cur);
+                if(!mp.containsKey(next)|| mp.get(next) > temp.moves){ //|| mp.get(next) > temp.moves
+                    mp.put(next, temp.moves);
+                    pq.insert(temp);
+                }
+
+            }
+        }
+        return null;
+    }
+}
